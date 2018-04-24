2f509bb4ce509da666a93de1502bfff29fc73a68
Aliaksei Plashchanski <aleksey.ploschanskiy@gmail.com>
2018-04-24 13:44:02 +0300

    libcurl has been added to dependencies

---
+++ src/curl.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2018 The COLX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CURL_H
+#define BITCOIN_CURL_H
+
+#include <string>
+
+typedef std::string CUrl;
+
+/**
+ * Extract URL to redirect to.
+ * @param[url] input http(s) address
+ * @param[redirect] redirect http(s) address
+ * @param[out] error error description on fail
+ * @return true - success, false - fail
+ */
+bool GetRedirect(const CUrl& url, CUrl& redirect, std::string& error);
+
+#endif // BITCOIN_CURL_H
